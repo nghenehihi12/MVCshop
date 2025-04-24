9d9ea552f4e452faab1852a646954e206c2d0e64
MINH HOANG <mhoang12082006@gmail.com>
2025-04-24 08:00:55 +0700

    huong dan

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+    'base' => '/PTIT24CNTT01/ss21/',
+    'assets' => '/PTIT24CNTT01/ss21/assets/',
+    'baseURL' => 'http://localhost/PTIT24CNTT01/ss21/', 
+    'db' => array(
+        'host'     => 'localhost',
+        'name'     => 'ProductDB',
+        'username' => 'root',
+        'password' => ''
+    )
+);
