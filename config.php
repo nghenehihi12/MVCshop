bbfd993f4e5bf82f21d3c49d75433746589dc261
MINH HOANG <mhoang12082006@gmail.com>
2025-04-24 08:27:46 +0700

    change config to new folder path

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 return array(
-    'base' => '/PTIT24CNTT01/ss21/',
-    'assets' => '/PTIT24CNTT01/ss21/assets/',
-    'baseURL' => 'http://localhost/PTIT24CNTT01/ss21/', 
+    'base' => '/MVCshop/',
+    'assets' => '/MVCshop/assets/',
+    'baseURL' => 'http://localhost/MVCshop/', 
     'db' => array(
         'host'     => 'localhost',
         'name'     => 'ProductDB',
